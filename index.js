fcc31d1af32851457e7e53c7ee0c5fde47f4bb2d
Shuvo Deb <shuvodeb17014@gmail.com>
2023-05-02 03:28:59 +0600

    install cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
 
 const foodRecipe = require('./data/foodRecipe.json');
 
